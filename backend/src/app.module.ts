58fa6017dc338f3c815791d7b72c6259d0730b9b
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-04-22 16:47:35 +0700

    connect DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TestConnectionService } from './config/testConection';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+      }
+    ),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TestConnectionService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TestConnectionService } from './config/testConection';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const testConnectionService = app.get(TestConnectionService);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  console.log('Testing database connection...');
+  await testConnectionService.testConnection();
+  await app.listen(process.env.PORT ?? 9999);
 }
+
+console.log(`Server running on ${process.env.PORT ?? 9999}`);
 bootstrap();
